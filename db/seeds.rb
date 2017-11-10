4c9452406160f580f8cddd4859e9969c4c1e753d
Diego Alcocer <diegoaalcocer@gmail.com>
2017-11-10 20:59:33 +0000

    Implemented seed data for StudentDatum

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+StudentDatum.create!(
+  languages: "Language",
+  hourly_rate: "13.50",
+  active: "0",
+  position_wanted: "0",
+  )
